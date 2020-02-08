c82ae00b4e22d8ce3e75497b64eacbfaf416bcd3
ElvishLegion <39751373+ElvishLegion@users.noreply.github.com>
2020-02-08 12:17:35 -0600

    Add files via upload

---
+++ Module one.py
@@ -0,0 +1,50 @@
+print("GAME OVER!")
+print(" 'NO!' You slam your fist on the table as the words scroll across your computer screen. You lost again!")
+print("But you are getting better. I almost made it to level 5 that time you congratulate yourself.")
+print("This Class trip to the Hall of Incredible Science is so much better than being in school. You figured a mueseum ")
+print("would be boring. But the hall is full of all kinds of gadgets, nature displays and other cool stuff. The best part")
+print("is the Thinking Machine Room. It's got incredible video games -including one based on your favorite movie of all time")
+print("Operation Buzzard.")
+print
+print(" They just announced that it's time for the bus to take you back to school. Most of your class ")
+print("has already headed down stairs. But you cant resist playing one more game. You punch the start button" )
+print("You punch the START button. You hear the opening notes. A Jungle scene appears in front of you.")
+print("Suddenly the screen goes blank")
+print("Huh? Did the computer just crash")
+print("Then a message appears on the screen")
+print("'Help me! The super computer has taken over!'")
+print("'What? you mutter. Is this one of the other kids from your class playing some kind of joke on you? You glance")
+print("around . There's not one else in the room. Oh, well. You have to go anyway")
+print
+print("You leave the room and step into the open elevator. But when the doors slide shut, it doesn't move")
+print("You punch the buttons. You punch the buttons again. Nothing happens. You're trapped!")
+print("Don't panic you tell yourself. You grab the emergency phone. 'Hello? Hello?' you shout into the reciever.")
+print("A strange, deep voice answers. 'You have learned my secret. Now you cannot leave the Hall of Incredible Science alive!.'")
+print("'What secret?' you gasp. 'Who are you?' 'I am the Super Computer,' the voice declares.")
+print("'I have taken over this entire building. But since you got that message, you know too much. Now you must be destroyed!'")
+print("'Wait!' You plead. 'I thought that message was a joke! I won't tell anybody! I promise'")
+print("The voice just laughs. Then the elevator jolts into motion. You have a feeling you won't  like where it taking you.")
+print
+print
+print
+print
+
+
+print('''INSTRUCTIONS''')
+print("You are so doomed. You're trapped in the Hall of Incredible science. An Evil computer has taken over, and you know its secret.")
+print("Now it wats to silence you --- forever")
+print("The hall is filled with dangers. Every exhibit has been turned into a deadly menace. Even the building itself is out to get you")
+print("There's only one way to get out of the building To Find it, you'll have to make all the right choices. ")
+print("Make a single mistake ---- and you'l never see the light of day again!")
+print("Let's face it, You probably won't last five minutes")
+
+print("The elevator finall stops on the top floor of the Hall fo Incredible science. The doors slide open. You step out and ")
+print("imeediately the doors whiz shut behind you. You gaze around.")
+print("There's no one here. The whole hall must be empty by now. But the back of your neck prickles.")
+print("You can feel an evil presence watching you. How are you going to escape")
+door_choice=input("Which door do you want to choose? 1=Try the elevator again or 2=Look for a phone? ")
+if door_choice=="1":
+  print("You push the down button for the elevator. The doors open right away. Maybe things are back to normal. You start to step in But there's no car! Just an elevator shaft the goes all the way down. You frantically grab for the elevator cable. Will you ake it? Or will you plunge to your doom?")
+  choice_one=input("What do you want to do? 1=Accept or 2=Decline")
+if door_choice=="2":
+    print("This isnt done yet")
